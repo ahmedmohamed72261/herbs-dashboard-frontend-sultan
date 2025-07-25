e93498512827d2854b4acd729629b668479ead34
Ahmed Mohamed <albashengineerahmed@gmail.com>
2025-07-25 11:29:04 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,13 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  skipTrailingSlashRedirect: true,
   images: {
     domains: ['res.cloudinary.com'],
     unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+  basePath: '',
 };
 
 export default nextConfig;
